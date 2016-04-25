7f264eeafd21341daa8f4e26af4f665da8f983e3
Anton Zarubin <anton.zarubin@gmail.com>
2016-04-25 16:34:59 +0300

    Refactored skills table

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,15 @@ $(function () {
             scrollTop: $($(this).attr('href')).offset().top
         }, 500);
     });
+
+    var adjustLayout = function () {
+        var height = $(window).height();
+
+        $('#general').height(height);
+        $('#download').height(height);
+    };
+
+    $(window).resize(adjustLayout);
+
+    adjustLayout();
 });
